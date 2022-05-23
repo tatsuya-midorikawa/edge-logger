e14fbc1b89a07fd27ed5ab505a1412feab4d084a
midoliy <midoliy@outlook.jp>
2022-05-23 23:17:02 +0900

    update

--- src/Program.fs
+++ src/Program.fs
@@ -1,16 +1,40 @@
 ﻿open System.Text.Json
+open Microsoft.Win32
+open System.Web
 
 let inline wait<'T> (task: System.Threading.Tasks.Task<'T>) = System.Threading.Tasks.Task.WaitAll (task)
-let inline toJson<'T> (object: 'T) = JsonSerializer.Serialize(object, JsonSerializerOptions(WriteIndented = true))
+let inline toJson<'T> (object: 'T) = JsonSerializer.Serialize(object, JsonSerializerOptions(WriteIndented = true, Encoder = System.Text.Encodings.Web.JavaScriptEncoder.UnsafeRelaxedJsonEscaping, IgnoreNullValues = true))
 let output_to_winsrv = Logger.output Logger.winsrv'filepath
 
-let winsrv_json = 
-  Winsrv.getServices()
-  |> Winsrv.collect
-  |> toJson
+//let winsrv_json = 
+//  Winsrv.getServices()
+//  |> Winsrv.collect
+//  |> toJson
 
-task {
-  do! output_to_winsrv winsrv_json
-}
-|> wait
+//task {
+//  do! output_to_winsrv winsrv_json
+//}
+//|> wait
   
+
+//let HKLM'Edge = Registry.LocalMachine.OpenSubKey(@"SOFTWARE\Policies\Microsoft\Edge", false)
+//let values = HKLM'Edge.GetValueNames()
+//let edge'key = @"SOFTWARE\Policies\Microsoft\Edge"
+
+//HKLM'Edge.GetValueNames()
+//|> Array.map (fun name -> { Reg.Name = name; Reg.Value = Registry.GetValue(HKLM'Edge.Name, name, "") })
+//|> toJson
+//|> printfn "%s"
+
+//HKLM'Edge.GetValueNames()
+//|> Array.map (fun name -> { Reg.Name = name; Reg.Type = HKLM'Edge.GetValueKind(name) |> string; Reg.Value = Registry.GetValue(HKLM'Edge.Name, name, "") })
+
+Reg.getEdgeRegistries ()
+|> toJson
+|> printfn "%s"
+
+
+//let HKLM'Edge = Registry.LocalMachine.OpenSubKey(@"SOFTWARE\Policies\Microsoft\Edge", false)
+//HKLM'Edge.GetSubKeyNames() |> Array.iter (printfn "%s")
+
+//HKLM'Edge.Dispose()
---
+++ src/Reg.fs
@@ -0,0 +1,32 @@
+﻿module Reg
+
+open Microsoft.Win32
+
+type Reg = { Path: string; Name: string; Type: string; Value: obj; Values: Reg[]}
+
+[<Literal>]
+let edge'key = @"SOFTWARE\Policies\Microsoft\Edge"
+
+let inline getEdgeRegistries () =
+  let rec loop(key: RegistryKey) =
+    if key = null
+    then [||]
+    else
+      let values =
+        key.GetValueNames()
+        |> Array.map (fun name -> { Path = key.Name; Name = name; Type = key.GetValueKind(name) |> string; Value = Registry.GetValue(key.Name, name, ""); Values = null })
+      let children = 
+        key.GetSubKeyNames()
+        |> Array.map (fun name -> 
+          use key = key.OpenSubKey(name)
+          let xs = loop (key)
+          { Path = null; Name = name; Type = null; Value = null; Values = xs} )
+      Array.append values children
+    
+  use HKLM'Edge = Registry.LocalMachine.OpenSubKey(edge'key, false)
+  let hklm = loop(HKLM'Edge)
+
+  use HKCU'Edge = Registry.CurrentUser.OpenSubKey(edge'key, false)
+  let hkcu = loop(HKCU'Edge)
+
+  Array.append hklm hkcu
