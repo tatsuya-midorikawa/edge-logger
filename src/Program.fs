f4df5240f0c7c77c8fe95f058a4495b20d4f1d44
midoliy <midoliy@outlook.jp>
2022-06-27 21:46:05 +0900

    add logon user info task

--- src/Program.fs
+++ src/Program.fs
@@ -13,7 +13,7 @@ open System.Runtime.InteropServices
 
 let inline wait<'T> (task: System.Threading.Tasks.Task<'T>) = System.Threading.Tasks.Task.WaitAll (task)
 let inline toJson<'T> (object: 'T) = JsonSerializer.Serialize(object, JsonSerializerOptions(WriteIndented = true, Encoder = System.Text.Encodings.Web.JavaScriptEncoder.UnsafeRelaxedJsonEscaping, IgnoreNullValues = true))
-let emptyTask = System.Threading.Tasks.Task.Run<unit>(fun () -> ())
+let empty'task = System.Threading.Tasks.Task.Run<unit>(fun () -> ())
 
 type Command () =
   inherit ConsoleAppBase ()
@@ -23,33 +23,45 @@ type Command () =
     [<Option("w", "Output Windows services info.");Optional;DefaultParameterValue(false)>] winsrv: bool,
     [<Option("e", "Output Edge policy info.");Optional;DefaultParameterValue(false)>] edge: bool,
     [<Option("i", "Output internet option info.");Optional;DefaultParameterValue(false)>] ie: bool,
+    [<Option("u", "Output Logon user info.");Optional;DefaultParameterValue(false)>] usr: bool,
     [<Option("f", "Output full info.");Optional;DefaultParameterValue(false)>] full: bool) = 
     
-    let winsrvTask =
+    let winsrv'task =
       if full || winsrv then
         let path = Logger.winsrv'filepath dir
         Winsrv.getServices() |> Winsrv.collect |> toJson |> Logger.output path
       else
-        emptyTask
+        empty'task
 
-    let edgeTask =
+    let edge'task =
       if full || edge then
         let path = Logger.edge'filepath dir
         EdgePolicy.fetch () |> toJson |> Logger.output path
       else
-        emptyTask
+        empty'task
 
-    let ieTask =
+    let ie'task =
       if full || ie then
         let path = Logger.ie'filepath dir
         IEReg.getIeRegistries () |> toJson |> Logger.output path
       else
-        emptyTask
+        empty'task
+
+    let usr'task =
+      if full || usr then
+        task {
+          do! Cmd.dsregcmd |> Cmd.exec |> Logger.output (Logger.dsregcmd'filepath dir)
+          do! Cmd.whoami |> Cmd.exec |> Logger.output (Logger.whoami'filepath dir)
+          do! Cmd.cmdkey |> Cmd.exec |> Logger.output (Logger.cmdkey'filepath dir)
+        }
+      else
+        empty'task
 
     task {
-      do! winsrvTask
-      do! edgeTask
-      do! ieTask
+      do! winsrv'task
+      do! edge'task
+      do! ie'task
+      do! usr'task
     }
     |> wait
   
@@ -117,29 +129,29 @@ let main args =
   //|> Cmd.exec
   //|> printfn "%s"
 
-  task {
-    let path = Logger.dsregcmd'filepath dir
-    do! Cmd.dsregcmd
-        |> Cmd.exec
-        |> Logger.output path
-  }
-  |> wait
+  //task {
+  //  let path = Logger.dsregcmd'filepath dir
+  //  do! Cmd.dsregcmd
+  //      |> Cmd.exec
+  //      |> Logger.output path
+  //}
+  //|> wait
 
-  task {
-    let path = Logger.whoami'filepath dir
-    do! Cmd.whoami
-        |> Cmd.exec
-        |> Logger.output path
-  }
-  |> wait
+  //task {
+  //  let path = Logger.whoami'filepath dir
+  //  do! Cmd.whoami
+  //      |> Cmd.exec
+  //      |> Logger.output path
+  //}
+  //|> wait
 
-  task {
-    let path = Logger.cmdkey'filepath dir
-    do! Cmd.cmdkey
-        |> Cmd.exec
-        |> Logger.output path
-  }
-  |> wait
+  //task {
+  //  let path = Logger.cmdkey'filepath dir
+  //  do! Cmd.cmdkey
+  //      |> Cmd.exec
+  //      |> Logger.output path
+  //}
+  //|> wait
 
   0
 
