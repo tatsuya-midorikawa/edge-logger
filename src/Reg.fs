c55ea307579c475400376be57b5e5310a725ce2d
midoliy <midoliy@outlook.jp>
2022-07-07 08:41:18 +0900

    wip

--- src/Reg.fs
+++ src/Reg.fs
@@ -1,5 +1,6 @@
 ﻿module Reg
 
+open Microsoft.FSharp.Core.CompilerServices
 open Microsoft.Win32
 open System.Text.Json.Serialization
 open System.IO
@@ -10,17 +11,22 @@ type regkey = {
   with 
     static member create (key: RegistryKey) name =
       let inline get (key: RegistryKey, name) = Registry.GetValue(key.Name, name, "")
-      { name = name; value = get(key, name) }
+      { name = name; value = get(key, name) } :> obj
 
 type root = HKLM = 0 | HKCU = 1
 type dict<'T, 'U> = System.Collections.Generic.Dictionary<'T, 'U>
 type pair = System.Collections.Generic.KeyValuePair<string, obj> 
 
-let read (root: root, path: string) =
+let rec read (root: root, path: string) =
   let inline combine (s1, s2) = Path.Combine(s1, s2)
-  let inline replace (s: string) = s.Replace(@"\", @"/")
-  let gen = combine >> replace
-  use root = match root with root.HKLM -> Registry.LocalMachine | root.HKCU -> Registry.CurrentUser | _ -> Registry.LocalMachine
-  use key = root.OpenSubKey(path)
+
+  use r = match root with root.HKLM -> Registry.LocalMachine | root.HKCU -> Registry.CurrentUser | _ -> Registry.LocalMachine
+  use key = r.OpenSubKey(path)
+  let mutable c = ArrayCollector<obj>()
   let regs = key.GetValueNames() |> Array.map (regkey.create key)
-  dict<string, obj>([| pair(gen(root.Name, path), regs) |])
+  c.AddMany(regs)
+
+  for sub in key.GetSubKeyNames() do
+    let v = read (root, combine(path, sub))
+    c.Add(dict<string, obj>([| pair(sub, v) |]))
+  c.Close()
