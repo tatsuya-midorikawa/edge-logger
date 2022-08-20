5536f44c2e21ee6320170abe1ef3533c7efe998f
midoliy <midoliy@outlook.jp>
2022-08-20 22:57:28 +0900

    wip

---
+++ pwsh/IO.fs
@@ -0,0 +1,31 @@
+﻿module IO
+
+open System.IO
+open System.Text
+open System.Buffers
+
+let private enc = UnicodeEncoding()
+
+type str (stream: Stream) =
+  member __.read () =
+    let size = 256
+    let mutable str = StringBuilder(size)
+    let append (buf: array<byte>) = enc.GetString(buf) |> str.Append |> ignore
+    let rec read pos =
+      let buf = ArrayPool.Shared.Rent(size)
+      let len = stream.Read(buf, pos, size)
+      if size <> len then
+        let buf' = ArrayPool.Shared.Rent(len)
+        System.Array.Copy(buf, buf', len)
+        append(buf')
+        ArrayPool.Shared.Return(buf)
+        ArrayPool.Shared.Return(buf')
+      else
+        append(buf)
+        ArrayPool.Shared.Return(buf)
+        read (pos + len)
+
+    read 0
+    str.ToString()
+
+  member __.write msg = ()
---
+++ pwsh/Program.fs
@@ -0,0 +1,15 @@
+﻿open System.IO.Pipes
+open System.Diagnostics
+
+let pipe'name =
+  let pid = Process.GetCurrentProcess().Id
+  $"jp.dsi.pwsh.{pid}"
+
+[<EntryPoint>]
+let main args =
+  use pipe'srv = new NamedPipeServerStream(pipe'name, PipeDirection.InOut)
+  pipe'srv.WaitForConnection()
+
+  let stream = StreamString(pipe'srv)
+
+  0
