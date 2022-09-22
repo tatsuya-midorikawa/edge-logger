a086192ec14b84903dcdc282dbc9dcc5811226ba
midoliy <midoliy@outlook.jp>
2022-09-22 20:52:31 +0900

    rem

--- src/Winsrv.fs
+++
@@ -1,8 +0,0 @@
-﻿module Winsrv
-let a = ()
-
-//type Service = { name: string; start_type: string; status: string }
-//let inline getServices () =
-//  System.ServiceProcess.ServiceController.GetServices()
-//let inline collect (services: System.ServiceProcess.ServiceController[]) =
-//  services |> Array.map (fun s -> { name = s.DisplayName; start_type = $"{s.StartType}"; status = $"{s.Status}" })
