813301919c87650eb5ea1bbfffd583c23f7bb080
midoliy <midoliy@outlook.jp>
2022-09-26 06:48:14 +0900

    wip

--- edge-logger.test/experiment.fs
+++ edge-logger.test/experiment.fs
@@ -5,6 +5,33 @@ open Xunit
 open Xunit.Abstractions
 open jp.dsi.logger.tools
 
+[<AutoOpen>]
+module F =
+  [<Struct;System.Runtime.CompilerServices.IsByRefLike;>]
+  type s<'a> = { head: 'a; tail: Span<'a> }
+
+  [<Struct;System.Runtime.CompilerServices.IsByRefLike;>]
+  type t<'a> =
+    | Empty
+    | Some of s<'a>
+
+  let split (xs: Span<_>) =
+    match xs.Length with
+    | 0 -> t.Empty
+    | 1 -> t.Some { head = xs[0]; tail = Span<_>.Empty }
+    | _ -> t.Some { head = xs[0]; tail = xs.Slice(1) }
+    
+  //[<Struct;System.Runtime.CompilerServices.IsByRefLike;>]
+  //type t'<'a> =
+  //  | Empty
+  //  | Some of ('a * Span<'a>)
+
+  //let split' (xs: Span<_>) =
+  //  match xs.Length with
+  //  | 0 -> t'.Empty
+  //  | 1 -> t'.Some (xs[0], Span<_>.Empty)
+  //  | _ -> t'.Some (xs[0], xs.Slice(1))
+    
 type experiment (output: ITestOutputHelper) =
   let log msg = output.WriteLine $"{msg}"
 
@@ -24,3 +51,17 @@ type experiment (output: ITestOutputHelper) =
     match xs with
     | Empty -> log "empty"
     | Some (head, tail) -> tail |> Array.iter log
+
+  [<Fact>]
+  member __.``exp.002`` () =
+    let xs = [| 0; 1; 2; 3; 4; 5 |]
+    let span = xs.AsSpan()
+
+    match split span with
+    | t.Empty -> log "empty"
+    | t.Some x -> x.tail.ToArray() |> Array.iter log
+    //| t.Some x -> x.tail.ToArray() |> Array.iter log
+    
+    //match split' span with
+    //| t'.Empty -> log "empty"
+    //| t'.Some (head, tail) -> tail.ToArray() |> Array.iter log
